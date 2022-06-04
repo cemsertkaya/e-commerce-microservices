f42e07a66d6d586f34069baee08ecc634d465aed
Cem <cemsertkaya99@gmail.com>
2022-06-04 20:07:40 +0300

    All artifacts are implemented

--- user-service/src/main/java/com/archTech/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/archTech/userservice/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.archTech.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
